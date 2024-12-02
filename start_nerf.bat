7ed59b5ef683a4f60e6cd0c87c2d0466aaf21ae9
Vadim Lebedev <vadiml1024@gmail.com>
2024-12-02 21:57:45 +0100

    Update start_nerf.bat to launch Nerf Admin with Streamlit

--- start_nerf.bat
+++ start_nerf.bat
@@ -5,7 +5,7 @@ echo Starting Nerf Gun Control System...
 call .venv\Scripts\activate.bat
 
 :: Start both applications
-start cmd /k python nerf-admin\nerf-admin.py
+start cmd /k streamlit run nerf-admin\nerf-admin.py
 start cmd /k python nerf-gun-control\main.py
 
 echo Applications started in separate windows. 
