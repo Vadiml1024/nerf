48e32c068619fba90d309e45b62232ea4d7da630
Vadim Lebedev <917273+Vadiml1024@users.noreply.github.com>
2025-06-05 00:33:41 +0300

    Use 'shot' key in params

--- nerf-gun-control/nerf_controller.py
+++ nerf-gun-control/nerf_controller.py
@@ -10,7 +10,7 @@ class NerfController:
 
     def fire(self, x=0, y=0, shot=1, wait=True):
         url = f"{self.server_url}/nerf"
-        params = {"x": x, "y": y, "shots": shot}
+        params = {"x": x, "y": y, "shot": shot}
         try:
             response = requests.get(url, params=params)
             response.raise_for_status()
